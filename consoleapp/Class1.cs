29d9b1b5156f89d4dcfb9a32f5d564f79fd19238
Yuta Matsumura <tkmbmbr@gmail.com>
2026-02-22 06:05:31 +0000

    コードのインデントを修正し、可読性を向上

--- consoleapp/Class1.cs
+++ consoleapp/Class1.cs
@@ -1,13 +1,13 @@
-namespace consoleapp;
+    namespace consoleapp;
 
 internal class Class1
-{
-    private string field1 = "hoge";
-
-    public string Property1 => field1;
-
-    public string Greet()
     {
-        return $"Hello, World {Property1}!"; // comment
+        private     string      field1  =   "hoge";
+
+    public  string  Property1
+        =>  field1  ;
+
+    public string   Greet     ()  {
+            return  $"Hello, World {  Property1  }!"     ;   // comment
+        }
     }
-}
